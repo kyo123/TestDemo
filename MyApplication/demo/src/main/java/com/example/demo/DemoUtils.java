d8524568591eed857ced8d2b9186c2ac5cedad69
kyo123 <823963923@qq.com>
2018-10-15 17:03:04 +0800

    hello

---
+++ MyApplication/demo/src/main/java/com/example/demo/DemoUtils.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import android.util.Log;
+
+/**
+ * Created by kyo on 18-10-15.
+ */
+
+public class DemoUtils {
+    public void hello(){
+        Log.e("hello","hello");
+    }
+}
